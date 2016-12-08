d12137428384173fd4512403579d011cb853ec3f
Muhammad Labib Ramadhan <mlabib.ramadhan@gmail.com>
2016-12-09 02:49:19 +0700

    Simplified index.js

--- src/index.js
+++ src/index.js
@@ -2,11 +2,6 @@ process.setMaxListeners(0);
 
 require('./core/setup/globals');
 
-const run = async () => {
-  // boot the server
-  const ServerBoot = requireF('core/services/boot/ServerBoot');
-  const serverBoot = new ServerBoot();
-  await serverBoot.boot();
-};
-
-run();
+const ServerBoot = requireF('core/services/boot/ServerBoot');
+const serverBoot = new ServerBoot();
+serverBoot.boot();
